14b4373207c6a9f22ac07f1814bfb27c7df7ead8
zhuangm <zhuangm@UKMAN1NB10042>
2016-07-18 14:27:35 +0100

    Add project files.

---
+++ CodeKataPractice/StringCalculate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace CodeKataPractice
+{
+    public class StringCalculate
+    {
+         public int Add(string s)
+         {
+            int rt = 0;
+            if(s == string.Empty)
+            {
+                rt = 0;
+            }
+            else
+            {
+              if(int.TryParse(s,out rt))
+                {
+                    if (rt < 0)
+                    {
+                        throw new System.ArgumentException("Can't be negative number");
+                    }
+                    else
+                    {
+                        if(rt >1000)
+                        {
+                            return 0;
+                        }
+                        else
+                        {
+                            return rt;
+                        }                        
+                    }
+                }   
+              else
+                {
+                    string[] separate = { ",", "#"};
+                    string[] separates = s.Split(separate, StringSplitOptions.RemoveEmptyEntries);
+                    int i;
+                    foreach(var st in separates)
+                    {
+                        if (int.TryParse(st, out i))
+                        {
+
+                            rt = rt+ Convert.ToInt32(st);
+                        }
+                    }
+                    if (rt > 1000)
+                        rt = 0;
+                }           
+            }
+                return rt;
+        }
+    }
+}
---
+++ StringCalculate_UnitTest/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeKataPractice;
+
+namespace StringCalculate_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        [TestMethod]
+        public void TestEmptyString()
+        {
+            var sc = new StringCalculate();
+            Assert.AreEqual(0, sc.Add(string.Empty));
+        }
+
+        [TestMethod]
+        public void TestSingleNumber()
+        {
+            var sc = new StringCalculate();
+            Assert.AreEqual(5, sc.Add("5"));
+            Assert.AreEqual(0, sc.Add("0"));
+        }
+
+        [TestMethod]
+        public void TestCommaDelimitNumber()
+        {
+            var sc = new StringCalculate();
+            Assert.AreEqual(11, sc.Add("5,6"));
+        }
+
+        [TestMethod]
+        public void TestSingleCharDelimitNumber()
+        {
+            var sc = new StringCalculate();
+            Assert.AreEqual(11, sc.Add("5#6"));
+        }
+
+        [TestMethod]
+        public void TestDoubleCharDelimitNumber()
+        {
+            var sc = new StringCalculate();
+            Assert.AreEqual(5, sc.Add("5#£"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestNegativeNumber()
+        {
+            var sc = new StringCalculate();
+            int i = sc.Add("-5");
+        }
+
+        [TestMethod]
+        public void TestMaxiNumber()
+        {
+            var sc = new StringCalculate();
+            Assert.AreEqual(0, sc.Add("1001"));
+        }
+
+        [TestMethod]
+        public void TestMaxiTotalNumber()
+        {
+            var sc = new StringCalculate();
+            Assert.AreEqual(0, sc.Add("500,501"));
+        }
+    }
+}
